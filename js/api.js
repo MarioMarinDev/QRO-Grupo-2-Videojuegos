88f7eb200fd925a64ced34e98446896de98d9e5a
Mario Marín <MarioMarin_25@outlook.com>
2022-07-14 16:39:33 -0500

    Cambiar indice de juego mendiante botones

--- js/api.js
+++ js/api.js
@@ -1,5 +1,6 @@
 
 const uri = "https://raw.githubusercontent.com/MarioMarinDev/QRO-Grupo-2-API/master/";
+let indice_juego = 0;
 let juego_seleccionado = null;
 
 // Encontrar elementos del HTML
@@ -8,6 +9,8 @@ const el_caja = document.getElementById("caja");
 const el_titulo = document.querySelector("div#juego-info h2");
 const el_genero = document.querySelector("div#juego-info h3");
 const el_desc = document.querySelector("div#juego-info p");
+const el_nav_anterior = document.querySelector("#juego-nav button:first-child");
+const el_nav_siguiente = document.querySelector("#juego-nav button:last-child");
 
 fetch(uri + "datos.json").then(function(respuesta) {
   return respuesta.json();
@@ -15,10 +18,13 @@ fetch(uri + "datos.json").then(function(respuesta) {
   for(let i = 0; i < datos.length; i+=1) {
     // Crear nuevo botón y agregarlo a la lista
     let nuevo_boton = document.createElement("button");
+    nuevo_boton.setAttribute("data-number", i);
     nuevo_boton.innerHTML = datos[i].nombre;
     el_lista.appendChild(nuevo_boton);
     // Agregar el evento de clic al botón
     nuevo_boton.addEventListener("click", function() {
+      // Cambiar índice de juego seleccionado
+      indice_juego = nuevo_boton.getAttribute("data-number");
       // Cambiar caja del juego, titulo y genero
       el_caja.src = uri + datos[i].imagen;
       el_titulo.innerHTML = datos[i].nombre;
@@ -47,4 +53,23 @@ fetch(uri + "datos.json").then(function(respuesta) {
   }
 }).catch(function(error) {
   console.log(error);
+});
+
+// Agregar eventos de navegación (anterior y siguiente)
+el_nav_anterior.addEventListener("click", function() {
+  if(indice_juego <= 0) {
+    return;
+  }
+  indice_juego -= 1;
+  let botones = document.querySelectorAll("#lista-juegos button");
+  botones[indice_juego].click();
+});
+
+el_nav_siguiente.addEventListener("click", function() {
+  let botones = document.querySelectorAll("#lista-juegos button");
+  if(indice_juego >= botones.length - 1) {
+    return;
+  }
+  indice_juego = parseInt(indice_juego) + 1;
+  botones[indice_juego].click();
 });
